d6bb00cc984b8505ddc9babd921652e301e2e7fc
Oayobami <164595316+Oayobami@users.noreply.github.com>
2024-04-13 22:04:25 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,51 @@
+# Name: Oluwadeyi Ayobami
+# File Name: app.py
+# Description: Backend web API using flask to deliver book data
+
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(80), nullable=False)
+    publisher = db.Column(db.String(80), nullable=False)
+
+
+    def __repr__(self):
+        return f"Name: {self.book_name} - Author: {self.author} - Publisher: {self.publisher}"
+    
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return 'Hello!'
+
+@app.route('/books')
+def get_books():
+    books = Book.query.all()
+
+    output = []
+    for book in books:
+        book_data = {'book_name': book.book_name, 'author': book.author, 'publisher': book.publisher}
+
+        output.append(book_data)
+
+    return {"books": output}
+
+@app.route('/books/<id>')
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return {"book_name": book.book_name, "author": book.author, 'publisher': book.publisher}
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    book = Book(book_name=request.json['book_name'], author=request.json['author'], publisher=request.json['publisher'])
+    db.session.add(book)
+    db.session.commit()
+    return {'id': book.id}
